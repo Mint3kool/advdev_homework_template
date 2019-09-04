62c641b3e416cd917688da8bfab488694b246f87
Tay Shen <tshen@redhat.com>
2019-09-04 15:12:35 -0400

    Dev deployment stage filled in

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -13,7 +13,7 @@ CLUSTER=$3
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
 # Set up Jenkins with sufficient resources
-oc new-project ${GUID}-jenkins --display-name "${GUID} Homework Jenkins"
+#oc new-project ${GUID}-jenkins --display-name="${GUID} Homework Jenkins"
 
 oc new-app jenkins-persistent --param ENABLE_OAUTH=false --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true
 
